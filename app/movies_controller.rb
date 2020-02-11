23503c06c8585735212ff4cba0c99973c121bf2e
Daniel Reina <dannyflatiron@gmail.com>
2020-02-11 18:18:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,13 +97,12 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  Movie.where(title: "That One Where the Guy Kicks Another Guy Once").destroy_all
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all
 end
